b8abdbf70922173f3950b0f235533f53bbe115ac
Vajira Priyangani <87774312+vajira-priyangani@users.noreply.github.com>
2023-05-16 16:12:17 +0530

    add feedback to models folder

---
+++ backend/models/Feedback.js
@@ -0,0 +1,9 @@
+const mongoose = require('mongoose');
+const feedbackSchema = new mongoose.Schema ({
+    fullName:{type:String,required:true},
+    email:{type:String,required:true},
+    feedBack:{type:String,required:true},
+ 
+});
+ 
+module.exports = mongoose.model('feedback',feedbackSchema);
